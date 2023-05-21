cf5852eb562745851a716466f411a738b73f2fa8
t3rixis <spons0rr1703@gmail.com>
2023-05-21 21:16:37 +0300

    adding cameras

--- CarMovement/Assets/CameraController.cs
+++ CarMovement/Assets/CameraController.cs
@@ -1,19 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController : NetworkBehaviour
 {
-    [SerializeField] private Transform _car;
+    private NetworkTransform _carTransform;
     private Vector3 _offset = new Vector3(0f, 2f, -4f);
     private float _speed = 10f;
 
+    private void Start()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        // Находим объект с помощью кода
+        GameObject carObject = GameObject.Find("Car");
+        _carTransform = carObject.GetComponent<NetworkTransform>();
+    }
+
     private void FixedUpdate()
     {
-        var targetPosition = _car.TransformPoint(_offset);
+        if (!isLocalPlayer || _carTransform == null)
+            return;
+
+        var targetPosition = _carTransform.transform.TransformPoint(_offset);
         transform.position = Vector3.Lerp(transform.position, targetPosition, _speed * Time.deltaTime);
 
-        var direction = _car.position - transform.position;
+        var direction = _carTransform.transform.position - transform.position;
         var rotation = Quaternion.LookRotation(direction, Vector3.up);
 
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, _speed * Time.deltaTime);
